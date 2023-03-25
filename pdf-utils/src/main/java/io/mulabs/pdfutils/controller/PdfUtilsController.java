7bffd86e5bab3303ed30a6eca9accb28948a4a25
Jhinuk <kundupapu102@yahoo.com>
2023-03-25 21:34:50 +0200

    PDF Merge

--- pdf-utils/src/main/java/io/mulabs/pdfutils/controller/PdfUtilsController.java
+++ pdf-utils/src/main/java/io/mulabs/pdfutils/controller/PdfUtilsController.java
@@ -1,20 +1,25 @@
 package io.mulabs.pdfutils.controller;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.URI;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import org.apache.pdfbox.io.MemoryUsageSetting;
 import org.apache.pdfbox.multipdf.PDFMergerUtility;
+import org.apache.pdfbox.multipdf.PDFMergerUtility.DocumentMergeMode;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,10 +29,10 @@ import org.springframework.web.multipart.MultipartFile;
 @RequestMapping("/pdf")
 public class PdfUtilsController {
 
-	@RequestMapping("/merge")
+	@PostMapping("/merge")
 	public ResponseEntity<byte[]> merge(@RequestParam("files") MultipartFile[] files) {
 
-		List<PDDocument> PDDocumentList = Stream.of(files).map(file -> {
+		List<PDDocument> PDDocumentList = Arrays.stream(files).map(file -> {
 			try {
 				return PDDocument.load(file.getInputStream());
 			} catch (IOException e) {
@@ -50,15 +55,19 @@ public class PdfUtilsController {
 		String mergedPdf = "MergedPdf.pdf";
 
 		PDFMergerUtility pdfMergerUtility = new PDFMergerUtility();
+		pdfMergerUtility.setDocumentMergeMode(DocumentMergeMode.OPTIMIZE_RESOURCES_MODE);
 		pdfMergerUtility.setDestinationFileName(mergedPdf);
 		try {
+
 			for (MultipartFile multipartFile : files) {
 				pdfMergerUtility.addSource(multipartFile.getInputStream());
-				pdfMergerUtility.mergeDocuments(MemoryUsageSetting.setupMainMemoryOnly());
 			}
+
+			pdfMergerUtility.mergeDocuments(MemoryUsageSetting.setupTempFileOnly());
 
 		} catch (IOException e) {
 			// TODO
+			e.printStackTrace();
 		}
 
 		HttpHeaders headers = new HttpHeaders();
@@ -67,7 +76,8 @@ public class PdfUtilsController {
 		headers.setCacheControl("must-revalidate, post-check=0, pre-check=0");
 		ResponseEntity<byte[]> response = null;
 		try {
-			response = new ResponseEntity<>(Files.readAllBytes(Path.of(URI.create(mergedPdf))), headers, HttpStatus.OK);
+			response = new ResponseEntity<>(Files.readAllBytes(Paths.get(mergedPdf)), headers,
+					HttpStatus.OK);
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
