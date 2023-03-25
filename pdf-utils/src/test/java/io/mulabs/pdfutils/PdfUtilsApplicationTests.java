a6c715dcaf2cfe658b0d508ee8ada06e0ba941e2
Jhinuk <kundupapu102@yahoo.com>
2023-03-25 19:58:54 +0200

    Initial Commit

---
+++ pdf-utils/src/test/java/io/mulabs/pdfutils/PdfUtilsApplicationTests.java
@@ -0,0 +1,13 @@
+package io.mulabs.pdfutils;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PdfUtilsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
